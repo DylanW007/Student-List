8f0fb3ed632b21520430b2474eefb7437e68aaab
U-BSD\453480 <453480@SHS-GYBF6X3.beaverton.k12.or.us>
2025-11-03 10:01:08 -0800

    new save

--- studentlist.cpp
+++ studentlist.cpp
@@ -1,36 +1,53 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 struct Student{
-  char name[10];
+  char firstname[20];
+  char lastname[20];
   int id;
   float gpa;
 };
 
+void print(){
+  //cout << struct(firstname)'s
+}
+
+char add(){
+
+  char all[1000] = "\0";
+  
+  Student student;
+
+  cout << "What is the student's first name?: ";
+  cin >> student.firstname;
+  cout << endl;
+
+  cout << "What is the student's last name?: ";
+  cin >> student.lastname;
+  cout << endl;
+
+  cout << "What is the student's id?: ";
+  cin >> student.id;
+  cout << endl;
+
+  cout << "What is the student's gpa?: ";
+  cin >> student.gpa;
+  cout << endl;
+
+  cout << "Your student is, " << student.firstname << " " << student.lastname << ", " << student.id << ", " << student.gpa << "." << endl;
+
+  all.push_back(student.firstname, student.lastname, student.id, student.gpa);
+  
+  return 0;
+}
+
+void remove(){
+  cout << "deleting";
+}
+
 int main(){
-  char studentname;
-  int studentid;
-  float studentgpa;
+  add();
   
-  cout << "What is your students name?: ";
-  cin >> studentname;
-  cout << "What is your students ID?: ";
-  cin >> studentid;
-  cout << "What is your students GPA?: ";
-  cin >> studentgpa;
-
-  Student studentname;
-  cin >> studentname.name;
-  studentname.id = studentid;
-  studentname.gpa = studentgpa;
-  cout << studentname.name << " ID:" << studentname.id << " GPA:" << studentname.gpa << endl;
-  
-  //Student dylan;
-  //cin >> dylan.name;
-  //dylan.id = 1234;
-  //dylan.gpa = 3.0;
-  //cout << dylan.name << " ID:" << dylan.id << " GPA:";
-  //cout << dylan.gpa << endl;
-
   return 0;
 }
